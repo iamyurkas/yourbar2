d8a44f03db15dbd125dd9f3a2ffa290c76bd904a
Iurii <iamyurkas@gmail.com>
2025-11-10 18:50:51 +0000

    Fix image picker mediaTypes option

--- app/ingredient/create.tsx
+++ app/ingredient/create.tsx
@@ -126,7 +126,7 @@ export default function IngredientCreateScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: [ImagePicker.MediaTypeOptions.Images],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.8,
