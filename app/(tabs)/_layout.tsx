a942d8eff2f55f7e696455ad6cb644fcfc6b38c4
Iurii <iamyurkas@gmail.com>
2025-11-11 22:23:11 +0000

    Remove bottom tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,13 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { BottomBar } from '@/navigation/BottomBar';
-
 export default function TabLayout() {
   return (
     <Tabs
       initialRouteName="cocktails"
       screenOptions={{
         headerShown: false,
-        tabBar: (props) => <BottomBar {...props} />,
-        tabBarHideOnKeyboard: true,
+        tabBar: () => null,
       }}>
       <Tabs.Screen name="cocktails" options={{ title: 'Cocktails' }} />
       <Tabs.Screen name="shaker" options={{ title: 'Shaker' }} />
