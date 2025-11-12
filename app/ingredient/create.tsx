ba9dc43d12d941f3e8083ae84b804be2c9e98c72
Iurii <iamyurkas@gmail.com>
2025-11-12 14:04:21 +0000

    Fixed colors

--- app/ingredient/create.tsx
+++ app/ingredient/create.tsx
@@ -346,7 +346,7 @@ export default function CreateIngredientScreen() {
           style={[
             styles.imagePlaceholder,
             { borderColor: paletteColors.outline },
-            !imageUri && { backgroundColor: paletteColors.onSurfaceVariant },
+            !imageUri && { backgroundColor: paletteColors.surfaceVariant },
           ]}
           onPress={handlePickImage}
           android_ripple={{ color: `${paletteColors.surface}33` }}>
