7811cb876b7644bad2f84c2ea7ab8d552d74dd62
Iurii <iamyurkas@gmail.com>
2025-12-14 20:57:49 +0000

    Align cocktail rating stars to right edge

--- components/CocktailListRow.tsx
+++ components/CocktailListRow.tsx
@@ -190,6 +190,7 @@ const styles = StyleSheet.create({
   ratingPill: {
     flexDirection: 'row',
     alignItems: 'center',
+    alignSelf: 'flex-end',
     gap: 2,
     borderRadius: 12,
     paddingVertical: 2,
