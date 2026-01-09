a6bd97be61a4da2b39caa1575f35e33e3c71374e
Iurii <iamyurkas@gmail.com>
2026-01-09 14:32:24 +0000

    Fix shaker icon overlap

--- app/(tabs)/cocktails/create.tsx
+++ app/(tabs)/cocktails/create.tsx
@@ -2719,6 +2719,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
+    overflow: 'hidden',
   },
   methodOptionIconImage: {
     width: 18,
