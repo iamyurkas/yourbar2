95b739ee547b8df968f056c4be8857acfc5ba78e
Iurii <iamyurkas@gmail.com>
2026-01-12 22:15:36 +0000

    Refactor tab bar layout components

---
+++ components/tab-bar/TabBarIcon.tsx
@@ -0,0 +1,18 @@
+import { Image } from 'expo-image';
+import React from 'react';
+
+type TabBarIconProps = {
+  source: number;
+  color: string;
+  focused: boolean;
+};
+
+export function TabBarIcon({ source, color, focused }: TabBarIconProps) {
+  return (
+    <Image
+      source={source}
+      style={{ width: 24, height: 24, tintColor: color, opacity: focused ? 1 : 0.72 }}
+      contentFit="contain"
+    />
+  );
+}
