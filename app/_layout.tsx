3ec0dab512a7e4d159792d86bef31a1f520f5f68
Iurii <iamyurkas@gmail.com>
2026-01-13 00:33:03 +0000

    Fixed splash and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
-import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { PaperProvider } from '@/libs/react-native-paper';
-import 'react-native-reanimated';
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { Colors } from '@/constants/theme';
-import { InventoryProvider } from '@/providers/inventory-provider';
-import { UnsavedChangesProvider } from '@/providers/unsaved-changes-provider';
-import { getAppTheme } from '@/theme/theme';
+import { Colors } from "@/constants/theme";
+import { PaperProvider } from "@/libs/react-native-paper";
+import { InventoryProvider } from "@/providers/inventory-provider";
+import { UnsavedChangesProvider } from "@/providers/unsaved-changes-provider";
+import { getAppTheme } from "@/theme/theme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
@@ -34,7 +34,10 @@ export default function RootLayout() {
           <ThemeProvider value={navigationTheme}>
             <Stack>
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+              <Stack.Screen
+                name="modal"
+                options={{ presentation: "modal", title: "Modal" }}
+              />
             </Stack>
             <StatusBar style="dark" />
           </ThemeProvider>
