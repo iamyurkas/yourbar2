e7a736a4744baef70c4d524825499f33f2ca841a
Iurii <iamyurkas@gmail.com>
2026-01-25 23:06:42 +0000

    Rename New Era tag to Tiki

--- constants/cocktail-tags.ts
+++ constants/cocktail-tags.ts
@@ -5,7 +5,7 @@ export const BUILTIN_COCKTAIL_TAGS = [
   { id: 1, name: 'IBA Official', color: TAG_COLORS[9] },
   { id: 2, name: 'Unforgettables', color: TAG_COLORS[3] },
   { id: 3, name: 'Contemporary', color: TAG_COLORS[5] },
-  { id: 4, name: 'New Era', color: TAG_COLORS[7] },
+  { id: 4, name: 'Tiki', color: TAG_COLORS[7] },
   { id: 5, name: 'strong', color: TAG_COLORS[0] },
   { id: 6, name: 'moderate', color: TAG_COLORS[1] },
   { id: 7, name: 'soft', color: TAG_COLORS[12] },
