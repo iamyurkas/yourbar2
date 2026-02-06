6c0a23f3bd03cd8b0a3ba9b7067c63230c8f9453
Iurii <iamyurkas@gmail.com>
2026-02-06 02:32:17 +0000

    fixed Sentry

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import appJson from "./app.json";
 
 export default {
   ...appJson.expo,
+
+  plugins: [...(appJson.expo.plugins ?? []), "@sentry/react-native"],
+
   extra: {
     ...appJson.expo.extra,
     buildTime: new Date().toISOString(),
