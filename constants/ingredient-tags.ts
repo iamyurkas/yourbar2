f2ba1e43c761340f9b90abe7b18fccc7f907ae55
Iurii <iamyurkas@gmail.com>
2026-02-14 03:40:42 +0000

    Fixed tags

--- constants/ingredient-tags.ts
+++ constants/ingredient-tags.ts
@@ -7,8 +7,8 @@ export const BUILTIN_INGREDIENT_TAGS = [
   { id: 3, name: "Beer/Cider", color: TAG_COLORS[3] },
   { id: 4, name: "Bitters", color: TAG_COLORS[14] },
   { id: 5, name: "Syrup", color: TAG_COLORS[13] },
-  { id: 6, name: "Fruit/Veg & juice", color: TAG_COLORS[10] },
-  { id: 7, name: "Mixer", color: TAG_COLORS[9] },
+  { id: 6, name: "Mixer", color: TAG_COLORS[9] },
+  { id: 7, name: "Fruit/Veg & Juice", color: TAG_COLORS[10] },
   { id: 8, name: "Fridge/Pantry", color: TAG_COLORS[6] },
   { id: 9, name: "Other", color: TAG_COLORS[15] },
 ] as const;
